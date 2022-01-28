1ddfa702c9a98e47c2272c5bf5461b76da4680d0
Nathan-AUS <nathan.nguyen.au@gmail.com>
2022-01-29 02:14:10 +1100

    fix merge

--- Week 2/demo/script.js
+++ Week 2/demo/script.js
@@ -1,6 +1,15 @@
+<<<<<<< HEAD
 let btn = document.getElementById("button");
 
 btn.onclick = () => {
   console.log("going to scroll to top");
   window.scrollTo({ top: 0, behavior: "smooth" });
 };
+=======
+let btn = document.getElementById("button");
+
+btn.onclick = () => {
+  console.log("going to scroll to top");
+  window.scrollTo({ top: 0, behavior: "smooth" });
+};
+>>>>>>> 2e07c9ee900eeabc3a7b639e5c479fe4e289b12e
